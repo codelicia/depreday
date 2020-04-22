100965abfbf1bcaa2826c3c71f0e5ed292e34f96
Jefersson Nathan <malukenho.dev@gmail.com>
2020-04-22 12:43:21 +0200

    Better usage of git blame range
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- src/Git/Blame.php
+++ src/Git/Blame.php
@@ -5,14 +5,12 @@ declare(strict_types=1);
 namespace Malukenho\Depreday\Git;
 
 use Symfony\Component\Process\Process;
-use function var_dump;
 
 final class Blame
 {
     public function __invoke(string $fileRealPath, int $cursorPosition) : string
     {
-        // todo: refactor the $cursorPosition usage
-        $output = (new Process(['git', 'blame', '--date=short', '-L', $cursorPosition.','.$cursorPosition, '--', $fileRealPath]))
+        $output = (new Process(['git', 'blame', '--date=short', '-L', $cursorPosition.',+1', '--', $fileRealPath]))
             ->mustRun()
             ->getOutput();
 
