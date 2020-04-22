0fbbd2b6443781ef5197d7ac9527cf51aadcfefb
Jefersson Nathan <malukenho.dev@gmail.com>
2020-04-22 12:29:36 +0200

    Introduce UI/Message
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- src/Console/App.php
+++ src/Console/App.php
@@ -10,13 +10,13 @@ use Malukenho\Depreday\Bin\Grep;
 use Malukenho\Depreday\FileLine;
 use Malukenho\Depreday\Git\Blame;
 use Malukenho\Depreday\UI\Logo;
+use Malukenho\Depreday\UI\Message;
 use Malukenho\Depreday\UI\Phrases;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use function array_map;
 use function getcwd;
 use function preg_match;
-use function sprintf;
 
 final class App
 {
@@ -24,40 +24,33 @@ final class App
     {
         $grep        = new Grep();
         $gitBlame    = new Blame();
+        $message     = new Message();
         $currentDate = new DateTimeImmutable('now');
         $phrases     = new Phrases;
+        $currentDir  = getcwd();
 
         array_map([$output, 'writeln'], Logo::logo());
+
+        $message->findingDeprecation($output, $currentDir);
 
         // TODO: make it configurable?
         // Make it work as just information?
         // Configure it to fail when a certain amount of days pass? or configure it on the comment itself?
-        $ls          = new Find(getcwd(), ['php']);
+        $ls          = new Find($currentDir, ['php']);
         $listOfFiles = $ls(['vendor', 'var', 'cache', 'node_modules']);
-
-        $output->writeln("\nFinding deprecations in the directory: <info>" . getcwd() . "</info>\n\n");
 
         /** @var FileLine[] $files */
         $files = $grep('@deprecated', $listOfFiles);
 
         foreach ($files as $file) {
-            $cursorPosition = $file->line() + 1;
-            $blameOutput    = $gitBlame($file->getRealPath(), $cursorPosition);
+            $blameOutput    = $gitBlame($file->getRealPath(), $file->line());
 
             // TODO: move regex after getting the git blame output
             $regexForDateExtraction = '/.+\(.+(\d{4}-\d{2}-\d{2}) /';
             preg_match($regexForDateExtraction, $blameOutput, $matches);
             $lastChange = DateTimeImmutable::createFromFormat('Y-m-d', $matches[1]);
 
-            $diff = $lastChange->diff($currentDate);
-
-            if ($diff->days === 0) {
-                continue;
-            }
-
-            $output->writeln(sprintf('<info>%s</info>:<fg=yellow>%s</>', $file->getRealPath(), $cursorPosition));
-            $output->writeln(sprintf($phrases->random(), '<fg=red>' . $diff->days . '</>'));
-            $output->writeln('');
+            $message->deprecationFound($output, $file->getRealPath(), $file->line(), $phrases->random(), $lastChange->diff($currentDate));
         }
 
         return 0;
---
+++ src/UI/Message.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Malukenho\Depreday\UI;
+
+use DateInterval;
+use Symfony\Component\Console\Output\OutputInterface;
+use function sprintf;
+
+final class Message
+{
+    public function findingDeprecation(OutputInterface $output, string $directory) : void
+    {
+        $output->writeln("\nFinding deprecations in the directory: <info>" . $directory. "</info>\n\n");
+    }
+
+    public function deprecationFound(
+        OutputInterface $output,
+        string $fileRealPath,
+        int $cursorPosition,
+        string $phrase,
+        DateInterval $dateInterval
+    ) : void {
+        if ($dateInterval->days === 0) {
+            return;
+        }
+
+        $output->writeln(sprintf('<info>%s</info>:<fg=yellow>%s</>', $fileRealPath, $cursorPosition));
+        $output->writeln(sprintf($phrase, '<fg=red>' . $dateInterval->days . '</>'));
+        $output->writeln('');
+    }
+}
